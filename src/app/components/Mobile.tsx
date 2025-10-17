4191b1b0c280d6b9577ae11acacf5aabd9ff1c4f
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2025-10-17 14:07:54 +0900

    #4 Create MobileComponent and migrate inline styles to CSS

--- src/app/components/Mobile.tsx
+++ src/app/components/Mobile.tsx
@@ -1,5 +1,17 @@
 import styles from "../page.module.css";
+import Button from "./Button";
 
 export default function MobileComponent() {
-  return <div className={styles.name}>mobile SaruwatariYuta</div>;
+  return (
+    <>
+      <div className={styles.topContainer}>
+        <div style={{ fontSize: "6vh" }}>Yuta Saruwatari</div>
+        <div style={{ fontSize: "3vh" }}>Software Engineer</div>
+        <div className={styles.button}>
+          <Button href='desktop/works'>Works</Button>
+          <Button href='desktop/about'>About</Button>
+        </div>
+      </div>
+    </>
+  );
 }
