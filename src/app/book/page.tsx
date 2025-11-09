bc3416c3d2871dc87861bc542d1299c4d7905bdd
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2025-11-09 23:50:27 +0900

    #23 Optimize page animation time

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -5,30 +5,33 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Experience } from "./components/Experience";
 import { UI } from "./components/Ui";
+import styles from "./page.module.css";
 
 export default function Book() {
   return (
     <>
       <UI />
       <Loader />
-      <Canvas
-        shadows
-        camera={{ position: [0, 1, 4], fov: 42 }}
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100vw",
-          height: "100vh",
-          zIndex: 100,
-        }}
-      >
-        <group position-y={-0.1} position-x={0.2}>
-          <Suspense fallback={null}>
-            <Experience />
-          </Suspense>
-        </group>
-      </Canvas>
+      <div className={styles.book}>
+        <Canvas
+          shadows
+          camera={{ position: [0, 1, 4], fov: 42 }}
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            zIndex: 100,
+          }}
+        >
+          <group position-y={-0.1} position-x={0.2}>
+            <Suspense fallback={null}>
+              <Experience />
+            </Suspense>
+          </group>
+        </Canvas>
+      </div>
     </>
   );
 }
